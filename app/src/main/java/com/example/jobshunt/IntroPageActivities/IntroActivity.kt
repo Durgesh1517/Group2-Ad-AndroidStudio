62ff39fc5e88fa8bd987a4206b705a6a12ed7495
hemanshi69 <hemanshisavaliya797@gmail.com>
2024-03-17 04:02:34 -0400

    intro activity completed

---
+++ app/src/main/java/com/example/jobshunt/IntroPageActivities/IntroActivity.kt
@@ -0,0 +1,15 @@
+    package com.example.jobshunt.IntroPageActivities
+
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+
+    import com.example.jobshunt.R
+
+    class IntroActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_intro)
+
+        }
+    }
